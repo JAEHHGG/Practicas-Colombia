66429c3d85f5a809008f27a069b2fce9ec63bfe1
jaehhgg <jhernan83075@universidadean.edu.co>
2019-05-26 16:43:21 -0500

    pruebas

--- Session/Conexion.php
+++ Session/Conexion.php
@@ -1,8 +1,15 @@
 <?php
-$conexion = mysqli_connect("192.168.0.9", "server", "server", "Registro");
-if (!$conexion) {
-    echo 'Error de conexión en la base de datos'
+$enlace = mysqli_connect("192.168.0.9", "server", "server", "Registro");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
-else {
-    echo 'Conexión exitosa a la base de datos'
-}
+
+echo "Éxito: Se realizó una conexión apropiada a Registro." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+mysqli_close($enlace);
+?>
