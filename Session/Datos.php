cbf79c64022573ad4ba7cfdc97ad22a6a43bcb42
jaehhgg <jhernan83075@universidadean.edu.co>
2019-05-26 17:58:58 -0500

    Registro de usuarios

--- Session/Datos.php
+++ Session/Datos.php
@@ -1,5 +1,22 @@
 <?php
 
-include 'Conexion.php'
+include 'Conexion.php';
 
+$nombres = $_POST['nombres'];
+$apellidos = $_POST['apellidos'];
+$email = $_POST['email'];
+$contraseña = $_POST['contraseña'];
+$repetir-contraseña = $_POST['repetir-contraseña'];
+$documento = $_POST['documento'];
+
+$insertar = "INSERT INTO Usuario(nombres, apellidos, email, contraseña, repetir-contraseña, documento) VALUES ('nombres', 'apellidos', 'email', 'contraseña', 'repetir-contraseña', 'documento')";
+
+$resultado = mysqli_query($conexion, $insertar);
+if (!resultado) {
+    echo 'Error de registro';
+} else {
+    echo 'Registro exitoso';
+}
 ?>
+
+
