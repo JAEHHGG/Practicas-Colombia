b375e23d13d63530c3eb91588c51838c255f0ed6
jaehhgg <jhernan83075@universidadean.edu.co>
2019-06-08 12:24:11 -0500

    Arreglos en css prueba

--- Session/Empresa/Registro_Empresa.php
+++ Session/Empresa/Registro_Empresa.php
@@ -11,8 +11,8 @@
 		<link rel="shortcut icon" type="image/x-icon" href="../Img/LogotipoPracticasColombia.ico" >
 		<!--Bootstrap nav-->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="../css/LoginCss.css">
-		<link rel="stylesheet" href="../css/Styles2.css">
+		<link rel="stylesheet" href="/home/jaehhgg/Documentos/Fork/css/LoginCss.css">
+		<link rel="stylesheet" href="/home/jaehhgg/Documentos/Fork/css/Styles2.css">
 	</head>
 
 	<body>
